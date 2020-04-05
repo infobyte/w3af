1f57843cb5c3ab93dce99ddb4465b364be5529d7
rodriguezmatiass <rodriguez.matiass.dev@gmail.com>
2020-04-05 13:31:35 -0300

    decodestring() is a deprecated alias since Python 3.1, use decodebytes()

--- w3af/core/data/misc/base64_nopadding.py
+++ w3af/core/data/misc/base64_nopadding.py
@@ -36,7 +36,7 @@ def decode_base64(data):
     missing_padding = len(data) % 4
     if missing_padding != 0:
         data += b'=' * (4 - missing_padding)
-    return base64.decodestring(data)
+    return base64.decodebytes(data)
 
 
 def is_base64(data):
